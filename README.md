dd0ce9c332f2b4bf7b2e0bd095ee5bacdb5ecd60
Josh Rouwhorst <joshrouwhorst@users.noreply.github.com>
2023-06-11 13:02:21 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,95 +1,29 @@
-# Obsidian Sample Plugin
+# Style Importer
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+The Style Importer plugin lets you load a CSS file into your snippets folder from a URL. This provides an easy way to keep styles synced across multiple notebooks.
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+- [GitHub](https://github.com/joshrouwhorst/style-importer)
+- [Buy Me a Coffee](https://buymeacoffee.com/joshrouwhorst)
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+## Setup
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+When you first set up this plugin in a notebook you will want to go through these steps:
 
-## First time developing plugins?
+1. Open Settings.
+2. Go to Style Import settings.
+3. Set the URL where your CSS file is hosted.
+4. Go to Appearance settings.
+5. Scroll down to CSS Snippets.
+6. Click the Refresh button if you don't see a style-importer file.
+7. Toggle on the style-importer file.
 
-Quick starting guide for new plugin devs:
+## Hosting CSS
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+A quick way to get your CSS file hosted is to go to [GitHub Gists](https://gist.github.com) where you can create a gist. Create your CSS file there and then enter the URL to the **Raw** version of your file in the Style Import settings.
 
-## Releasing new releases
+## Internet Requests & File Changes
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+This plugin will only make requests to the URL(s) you provide it for CSS files. It then saves that file in your `.obsidian/snippets` folder under the filename `style-importer.css`. No other file manipulation is done by this plugin.
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
-
-## Adding your plugin to the community plugin list
-
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-## How to use
-
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+## Feedback
+If you have any feedback or questions you can submit an issue on the [GitHub project](https://github.com/joshrouwhorst/style-importer/issues).
--- main.ts
+++ main.ts
@@ -1,84 +1,62 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+const FILE_LOCATION = `.obsidian/snippets/style-importer.css`
+let lastUpdated = new Date();
 
-interface MyPluginSettings {
-	mySetting: string;
+function formatDate(date: Date) {
+	return date.toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true }).replace(',', '');
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface Settings {
+	styleUrl: string;
+	updateOnLoad: boolean;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: Settings = {
+	styleUrl: '',
+	updateOnLoad: true
+}
+
+export default class StyleImport extends Plugin {
+	settings: Settings;
+
+	// Primary functionality
+	async runStyleImport() {
+		if (this.settings.styleUrl) {
+			let content;
+
+			// Get the latest version of the style file
+			try {
+				content = await fetch(this.settings.styleUrl)
+			} catch (err) {
+				new Notice(`Style Import: Failed to fetch style file "${this.settings.styleUrl}"`);
+				return;
+			}
+
+			const text = await content.text();
+
+			// Make sure the snippets folder exists
+			try {
+				await this.app.vault.createFolder('.obsidian/snippets')
+			} catch { /* Snippets folder already exists */ }
+
+			// Write the style-import.css file
+			await this.app.vault.adapter.write(FILE_LOCATION, text);
+			lastUpdated = new Date();
+		}
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		if (this.settings.updateOnLoad) {
+			await this.runStyleImport();
+		}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new StyleImportSettingTab(this.app, this));
 	}
 
-	onunload() {
+	async onunload() {
 
 	}
 
@@ -91,47 +69,49 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class StyleImportSettingTab extends PluginSettingTab {
+	plugin: StyleImport;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: StyleImport) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Style Import Settings' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Style URL')
+			.setDesc('Enter the URL of the CSS file you want to import. Be sure to include http:// or https://. Example: https://example.com/style.css')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('URL')
+				.setValue(this.plugin.settings.styleUrl)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.styleUrl = value;
 					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Update On Load')
+			.setDesc('Update the style when the plugin first loads')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.updateOnLoad)
+				.onChange(async (value) => {
+					this.plugin.settings.updateOnLoad = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Update Style')
+			.setDesc(`Last updated: ${formatDate(lastUpdated)}`)
+			.addButton(button => button
+				.setButtonText('Reload')
+				.onClick(async () => {
+					await this.plugin.runStyleImport();
 				}));
 	}
 }
